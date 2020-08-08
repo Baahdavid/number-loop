fd91fff0a5582c3acf8a273df196df797ab21f64
Baah <baahdavid54@gmail.com>
2020-08-08 09:26:14 +0000

    Create even.js

---
+++ loop/js/even.js
@@ -0,0 +1,21 @@
+
+
+
+function even() {
+    for (var i = 0; i < 10; i++){
+        if ((i % 2) !== 1) document.write(i+ 'even number'+"<br>");
+    }
+}
+
+even()
+
+
+
+
+    
+var i=20;
+var sum=0
+
+sum=sum+i
+
+document.write(sum)
