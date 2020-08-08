d9bfd7cd39bf589c23b83ed1528a9f880ad092ed
Baah <baahdavid54@gmail.com>
2020-08-08 09:26:18 +0000

    Create odd.js

---
+++ loop/js/odd.js
@@ -0,0 +1,29 @@
+
+
+
+
+function odd() {
+    for (var i = 0; i < 10; i++){
+        if ((i % 2) !== 0) document.write(i+ 'odd number'+"<br>");
+    }
+
+
+
+}
+
+
+
+
+odd()
+
+
+    var i=25;
+    var sum=0
+    
+    sum=sum+i
+    
+    document.write(sum)
+    
+    
+        
+    
